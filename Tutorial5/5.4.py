47318da37fb6078426204107c840cb7839f8966e
Aljo-Jaison <117513351+Aljo-Jaison@users.noreply.github.com>
2025-04-05 00:22:28 +0530

    Update 5.4.py

--- Tutorial5/5.4.py
+++ Tutorial5/5.4.py
@@ -4,7 +4,6 @@ data = [[10, 20, 30], [40, 50, 60], [70, 80, 90]]
 label = ['A', 'B', 'C']
 
 df = pd.DataFrame(data, index=label, columns=[f'Col{i+1}' for i in range(len(data[0]))])
-df.to_excel("o/p.xlsx", index=True)
-
+df.to_excel("C:\\Users\\aljoj\\Desktop\\Python\\Tutorial5\\output3.xlsx", index=True)
 print("Data written to o/p.xlsx")
 
